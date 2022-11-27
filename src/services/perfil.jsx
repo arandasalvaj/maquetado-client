1c1c8e7ff3ac215c556c9b68098deb4e349a9002
hachetk <hachetk_5@outlook.com>
2022-11-26 21:09:25 -0300

    actualizacion kvn servicios

--- src/services/perfil.jsx
+++ src/services/perfil.jsx
@@ -1,5 +1,5 @@
 import axios from "axios"
-//const API_URL = 'https://tuinvernadero.xyz/' //URL DE PRODUCCION, SOLO CUANDO ESTA EN PRODUCCION.
-const API_URL = 'http://localhost:8000/' //URL LOCAL, SOLO DE DESARROLLO.
+const API_URL = 'https://tuinvernadero.xyz/' //URL DE PRODUCCION, SOLO CUANDO ESTA EN PRODUCCION.
+// const API_URL = 'http://localhost:8000/' //URL LOCAL, SOLO DE DESARROLLO.
 
 export const editarPerfil=async(perfil,id_usuario,token)=> await axios.patch(`${API_URL}v1/usuarios/${id_usuario}`,perfil,{headers: {'Authorization': token}}) 
